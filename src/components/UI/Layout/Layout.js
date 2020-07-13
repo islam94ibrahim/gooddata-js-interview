ea7ed2218a3a74a394bc11cea14c7163492bb7e0
Islam Ibrahim <islam94.ibrahim@gmail.com>
2020-07-13 10:34:46 +0200

    feat: add Layout component for general styling

---
+++ src/components/UI/Layout/Layout.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './Layout.module.css';
+
+const Layout = (props) => (
+  <div className={classes.Layout}>{props.children}</div>
+);
+
+export default Layout;
